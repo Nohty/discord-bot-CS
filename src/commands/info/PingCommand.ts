7e5f823de8f42426ab8ca673c53ee083bf43985c
daan-hautekiet <daan.hautekiet@student.howest.be>
2021-01-28 16:20:09 +0100

    added PingCommand

---
+++ src/commands/info/PingCommand.ts
@@ -0,0 +1,18 @@
+import { Message } from "discord.js";
+import DiscordClient from "../../client/client";
+import BaseCommand from "../../utils/structures/BaseCommand";
+
+export default class PingCommand extends BaseCommand {
+	constructor() {
+		super("ping", "Displays the Latency.", []);
+	}
+
+	async run(client: DiscordClient, message: Message, args: Array<string>) {
+		const m = await message.channel.send("Pong!");
+		m.edit(
+			`Pong! Latency is \`${
+				m.createdTimestamp - message.createdTimestamp
+			}ms\`. API Latency is \`${Math.round(client.ws.ping)}ms\`.`
+		);
+	}
+}
