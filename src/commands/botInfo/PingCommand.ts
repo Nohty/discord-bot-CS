b4dc2fedfec7b9a5c45369e5b966bd9f6f098c69
daan-hautekiet <daan.hautekiet@student.howest.be>
2021-01-30 20:29:06 +0100

    changed folder name

--- src/commands/botInfo/PingCommand.ts
+++ src/commands/botInfo/PingCommand.ts
@@ -4,7 +4,7 @@ import BaseCommand from "../../utils/structures/BaseCommand";
 
 export default class PingCommand extends BaseCommand {
 	constructor() {
-		super("ping", "info", [], "Displays the Latency.", ["ping"]);
+		super("ping", "botinfo", [], "Displays the Latency.", ["ping"]);
 	}
 
 	async run(client: DiscordClient, message: Message, args: Array<string>) {
