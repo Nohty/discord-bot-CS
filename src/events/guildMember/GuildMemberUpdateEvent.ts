969bf16ccaa94c591b069c0ca02a34396a981355
daan-hautekiet <daan.hautekiet@student.howest.be>
2021-02-09 18:00:14 +0100

    added boost message

---
+++ src/events/guildMember/GuildMemberUpdateEvent.ts
@@ -0,0 +1,24 @@
+// https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberUpdate
+import { GuildMember, MessageEmbed, TextChannel } from "discord.js";
+import BaseEvent from "../../utils/structures/BaseEvent";
+import DiscordClient from "../../client/client";
+import sendBoostMessage from "../../utils/functions/boostfn";
+
+export default class GuildMemberUpdateEvent extends BaseEvent {
+	constructor() {
+		super("guildMemberUpdate");
+	}
+
+	async run(
+		client: DiscordClient,
+		oldMember: GuildMember,
+		newMember: GuildMember
+	) {
+		sendBoostMessage(
+			oldMember,
+			newMember,
+			"805419992543658025",
+			"803993858030960640"
+		);
+	}
+}
